061b0551c29f3437940c86e8aecd3b55599bab04
Luan Pinto <luan.nelmo@hotmail.com>
2022-12-11 21:58:45 -0300

    Definindo o layout e consumindo o microsservico de produtos

--- GeekShopping.ProductAPI/Controllers/ProductController.cs
+++ GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -35,7 +35,7 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<ProductVO>> Create(ProductVO vo)
+        public async Task<ActionResult<ProductVO>> Create([FromBody] ProductVO vo)
         {
             if (vo == null)
             {
@@ -46,7 +46,7 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpPut]
-        public async Task<ActionResult<ProductVO>> Update(ProductVO vo)
+        public async Task<ActionResult<ProductVO>> Update([FromBody] ProductVO vo)
         {
             if (vo == null)
             {
